96043bfb47d408d50f2dd5c88342fcd130df65ef
Popov Gennadiy <gennadiy.popov.87@yandex.ru>
2018-06-06 17:26:38 +0300

    fix (build) Fixed build

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,8 @@ import thunkMiddleware from 'redux-thunk'
 
 const middlewares = compose(
 	applyMiddleware(thunkMiddleware),
-	window.__REDUX_DEVTOOLS_EXTENSION__ &&
-		window.__REDUX_DEVTOOLS_EXTENSION__(),
+	// window.__REDUX_DEVTOOLS_EXTENSION__ &&
+	// 	window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
 
 export default createStore(reducers, middlewares)
